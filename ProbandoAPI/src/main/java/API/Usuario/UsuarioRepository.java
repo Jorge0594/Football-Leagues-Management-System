56bfeceba95bab8606acc2a3ec2ef3f909f7eab6
rgolderos <r.golderos@alumnos.urjc.es>
2017-11-08 16:01:22 +0100

    New methods in  Miembros e incidencias
    New methods added in MiembroComite and Incidencia and configurate their security roles.

--- ProbandoAPI/src/main/java/API/Usuario/UsuarioRepository.java
+++ ProbandoAPI/src/main/java/API/Usuario/UsuarioRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface UsuarioRepository extends MongoRepository<Usuario, String> {
 
 	Usuario findByNombreUsuarioIgnoreCase(String nombreUsuario);
+
+	Usuario findById(String id);
 }
