5fd0a77fa8ef54c624918b5f5551fe6e7d845f93
rgolderos <r.golderos@alumnos.urjc.es>
2017-11-18 11:39:10 +0100

    @JsonView
    @JsonView added in Incidencia, Sancion and MiembroComite.  Incidencia saves the time  now.

--- ProbandoAPI/src/main/java/API/Incidencia/Incidencia.java
+++ ProbandoAPI/src/main/java/API/Incidencia/Incidencia.java
@@ -5,61 +5,76 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="Incidencias")		
 public class Incidencia {
-@Id
-private String id;
-private String tipo;
-private String idJugador;
-private String idPartido;
-
-			
-public Incidencia() {}
-
-public Incidencia(String id, String tipo, String jugador, String partido) {
-	super();
-	this.id = id;
-	this.tipo = tipo;
-	this.idJugador = jugador;
-	this.idPartido = partido;
-}
-
-public String getId() {
-	return id;
-}
+	
+	
+	@Id
+	private String id;
+	private String tipo;
+	private String idJugador;
+	private String idPartido;
+	private String minuto;
+	
+				
+	public Incidencia() {}
 
 
-public void setId(String id) {
-	this.id = id;
-}
+	public Incidencia(String id, String tipo, String idJugador, String idPartido, String minuto) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+		this.idJugador = idJugador;
+		this.idPartido = idPartido;
+		this.minuto = minuto;
+	}
+
+	public String getId() {
+		return id;
+	}
 
 
-public String getTipo() {
-	return tipo;
-}
+	public void setId(String id) {
+		this.id = id;
+	}
 
 
-public void setTipo(String tipo) {
-	this.tipo = tipo;
-}
+	public String getTipo() {
+		return tipo;
+	}
 
 
-public String getIdJugador() {
-	return idJugador;
-}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
 
-public void setIdJugador(String jugador) {
-	this.idJugador = jugador;
-}
-
-public String getIdPartido() {
-	return idPartido;
-}
-
-public void setIdPartido(String partido) {
-	this.idPartido = partido;
-}
+	public String getIdJugador() {
+		return idJugador;
+	}
 
 
+	public void setIdJugador(String idJugador) {
+		this.idJugador = idJugador;
+	}
 
 
+	public String getIdPartido() {
+		return idPartido;
+	}
+
+
+	public void setIdPartido(String idPartido) {
+		this.idPartido = idPartido;
+	}
+
+
+	public String getMinuto() {
+		return minuto;
+	}
+
+
+	public void setMinuto(String minuto) {
+		this.minuto = minuto;
+	}
+	
+	
 }
